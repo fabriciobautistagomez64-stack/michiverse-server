6d8e248fc83c6c0666da559ba665a91f680804cb
fabriciobautistagomez64-stack <fabriciobautistagomez64@gmail.com>
2026-06-08 10:23:19 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("Michiverse server");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Michiverse server online");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Michiverse server running on port " + PORT);
+});
